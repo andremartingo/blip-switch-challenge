aff66ba5b42f7625561b517ee82d4d548ba66a15
André Martingo <andremartingo@Andres-MacBook-Pro.local>
2018-02-16 16:13:27 +0000

    market id validation

--- src/com/ppbf/helpers/Setup.java
+++ src/com/ppbf/helpers/Setup.java
@@ -13,98 +13,98 @@ import java.util.*;
 
 public class Setup {
 
-    private static Scanner in = new Scanner(System.in);
+	private static Scanner in = new Scanner(System.in);
 
-    public static void startSandbox() throws FileNotFoundException {
-        System.out.println("Welcome to the Switch Programme Exercise @Blip");
+	public static void startSandbox() throws FileNotFoundException {
+		System.out.println("Welcome to the Switch Programme Exercise @Blip");
 
-        boolean quit = false;
+		boolean quit = false;
 
-        do {
-            printMenu();
+		do {
+			printMenu();
 
-            int menuItem = in.nextInt();
+			int menuItem = in.nextInt();
 
-            switch (menuItem) {
-                case 1:
-                    // readFromFile returns a List with each entry representing a line of the file.
-                    List<String> lines = readFromFile("resources/eventsWithDuplicates.csv");
+			switch (menuItem) {
+				case 1:
+					// readFromFile returns a List with each entry representing a line of the file.
+					List<String> lines = readFromFile("resources/eventsWithDuplicates.csv");
 
-                    System.out.print("Choose marketId:");
-                    long marketId = in.nextLong();
+					System.out.print("Choose marketId:");
+					long marketId = in.nextLong();
 
-                    System.out.println(Sandbox.ex1(lines, marketId));
+					System.out.println(Sandbox.ex1(lines, marketId));
 
-                    break;
-                case 2:
-                    // readFromFile returns a List with each entry representing a line of the file.
-                    lines = readFromFile("resources/eventsWithDuplicates.csv");
+					break;
+				case 2:
+					// readFromFile returns a List with each entry representing a line of the file.
+					lines = readFromFile("resources/eventsWithDuplicates.csv");
 
-                    System.out.println(Sandbox.ex2(lines));
+					System.out.println(Sandbox.ex2(lines));
 
-                    break;
-                case 3:
-                    // readFromFile returns a List with each entry representing a line of the file.
-                    lines = readFromFile("resources/eventsWithoutDuplicates.csv");
+					break;
+				case 3:
+					// readFromFile returns a List with each entry representing a line of the file.
+					lines = readFromFile("resources/eventsWithoutDuplicates.csv");
 
-                    BigDecimal TOTAL_MONEY = new BigDecimal("30.1");
-                    BigDecimal updatedMoney = new BigDecimal("0");
+					BigDecimal TOTAL_MONEY = new BigDecimal("30.1");
+					BigDecimal updatedMoney = new BigDecimal("0");
+					List<Long> removedMarkets = new ArrayList<>();
+					Map<Long, BigDecimal> bets = new HashMap<>();
+					List<Bet> listBet = new ArrayList<>();
+					listBet = parse(lines);
 
+					do {
+						System.out.println("Total Money:" + TOTAL_MONEY);
 
-                    List<Long> removedMarkets = new ArrayList<>();
+						System.out.println(lines);
+						boolean flag = false;
+						do {
+							System.out.print("Choose marketId:");
+							marketId = in.nextLong();
+							if (Sandbox.validateMarket(lines, marketId)) {
+								flag = true;
+							}
+						} while (flag == false);
+						System.out.print("Choose stake:");
+						BigDecimal stake = in.nextBigDecimal();
+						if (stake.compareTo(TOTAL_MONEY) < 1) {
+							updatedMoney = Sandbox.validateAndUpdateTotalMoney(lines, TOTAL_MONEY, marketId, stake);
+							if (updatedMoney != null) {
+								TOTAL_MONEY = updatedMoney;
+								Sandbox.addMarketAndStateToMap(bets, marketId, stake);
+							} else {
+								System.out.println("Error...");
+							}
+						} else {
+							System.out.println("You dont have that money");
+						}
 
-                    Map<Long, BigDecimal> bets = new HashMap<>();
-                    List<Bet> listBet = new ArrayList<>();
-                    listBet = parse(lines);
+					} while (TOTAL_MONEY.compareTo(BigDecimal.ZERO) > 0);
+					Random rand = new Random();
+					long winner = rand.nextInt(3) + 1;
+					BigDecimal profit = new BigDecimal("0");
+					winner = 1;
+					if (bets.containsKey(winner)) {
+						for (Bet b : listBet) {
+							if (b.event.market.id == winner) {
+								profit = profit.add(bets.get(winner).multiply(b.event.market.odds).subtract(bets.get(winner)));
+							}
+						}
+					}
 
-                    do {
-                        System.out.println("Total Money:" + TOTAL_MONEY);
+					System.out.println("Profit:");
+					System.out.println(profit);
 
-                        System.out.println(lines);
+					break;
+				case 0:
+					quit = true;
+					break;
+				default:
+					System.out.println("ERROR: Invalid choice.");
+			}
+		} while (!quit);
 
-                        System.out.print("Choose marketId:");
-                        marketId = in.nextLong();
-                        System.out.print("Choose stake:");
-                        BigDecimal stake = in.nextBigDecimal();
-                        if(stake.compareTo(TOTAL_MONEY)< 1) {
-                            updatedMoney = Sandbox.validateAndUpdateTotalMoney(lines, TOTAL_MONEY, marketId, stake);
-                            if (updatedMoney != null) {
-                                TOTAL_MONEY = updatedMoney;
-                                Sandbox.addMarketAndStateToMap(bets, marketId, stake);
-                            } else {
-                                System.out.println("Error...");
-                            }
-                        }else{
-                            System.out.println("You dont have that money");
-                        }
-
-                    } while (TOTAL_MONEY.compareTo(BigDecimal.ZERO) > 0);
-                    Random rand = new Random();
-                    int  winner = rand.nextInt(3) + 1;
-                    BigDecimal profit = new BigDecimal("0");
-                    winner = 1;
-
-                    for (Map.Entry<Long, BigDecimal> entry : bets.entrySet())
-                    {
-                       if(entry.getKey() == 1){
-                           for(Bet b : listBet){
-                               if(b.event.market.id == entry.getKey()){
-                                   profit = profit.add(entry.getValue().multiply(b.event.market.odds).subtract(entry.getValue()));
-                               }
-                           }
-                       }
-                    }
-                    System.out.println("Profit:");
-                    System.out.println(profit);
-                    break;
-                case 0:
-                    quit = true;
-                    break;
-                default:
-                    System.out.println("ERROR: Invalid choice.");
-            }
-        } while (!quit);
-
-        System.out.println("Bye-bye!");
-    }
+		System.out.println("Bye-bye!");
+	}
 }
